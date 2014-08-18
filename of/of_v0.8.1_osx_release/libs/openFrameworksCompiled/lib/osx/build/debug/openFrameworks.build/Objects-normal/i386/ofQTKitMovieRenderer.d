4d15bb0980d5b43d86917643186c687781499d1f
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-08-18 16:55:52 +0900

    Remove of

--- of/of_v0.8.1_osx_release/libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofQTKitMovieRenderer.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/watilde/Development/of_v0.8.1_osx_release/libs/openFrameworks/video/ofQTKitMovieRenderer.m \
-  ../../../../libs/openFrameworks/video/ofQTKitMovieRenderer.h
--- of/of_v0.8.1_osx_release/libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofVec4f.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/watilde/Development/of_v0.8.1_osx_release/libs/openFrameworks/math/ofVec4f.cpp \
-  ../../../../libs/openFrameworks/math/ofVec2f.h \
-  /Users/watilde/Development/of_v0.8.1_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
-  ../../../../libs/glew/include/GL/glew.h \
-  ../../../../libs/tess2/include/tesselator.h \
-  ../../../../libs/openFrameworks/math/ofVec3f.h \
-  ../../../../libs/openFrameworks/math/ofVec4f.h
